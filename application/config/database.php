71e110c43f8421699ebe552c6e8f5ca85ca3f8b6
ahmadfaqih73 <40154392+ahmadfaqih73@users.noreply.github.com>
2021-04-29 01:58:02 +0700

    axda
    zxzx

--- application/config/database.php
+++ application/config/database.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------
@@ -76,7 +76,7 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => '',
+	'username' => 'pkl',
 	'password' => '',
 	'database' => '',
 	'dbdriver' => 'mysqli',

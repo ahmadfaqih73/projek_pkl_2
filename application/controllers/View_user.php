5ed6338ca3e3ac85f58635872ade1d56ae52976d
ahmadfaqih73 <40154392+ahmadfaqih73@users.noreply.github.com>
2021-06-04 03:21:33 +0700

    pemisahan,header,footer, side,nav sudah terpisah benar

---
+++ application/controllers/View_user.php
@@ -0,0 +1,6 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class View_user extends CI_Controller {
+    
+}
---
+++ application/models/Full_user.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Full_user extends CI_Model
+{
+    
+}
